a627f2e1ab37b485596ecc287c0b41c0708f5fb9
elescuchante10-code <elescuchante10@gmail.com>
2026-02-08 09:11:24 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text
+from .db import Base
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=True)
+    description = Column(Text, nullable=True)
 
