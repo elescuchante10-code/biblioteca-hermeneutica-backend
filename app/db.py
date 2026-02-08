62be4a7b4a174416c5a0d1d871949d1d545f2354
elescuchante10-code <elescuchante10@gmail.com>
2026-02-08 11:10:18 -0500

    Create db.py for database configuration

--- app/db.py
+++ app/db.py
@@ -1,23 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Base declarativa
-Base = declarative_base()
-
-# Base de datos (SQLite por ahora)
 DATABASE_URL = "sqlite:///./biblioteca.db"
 
-# Engine
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
-# Sesión local
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-
+Base = declarative_base()
