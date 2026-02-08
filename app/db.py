c875c7ba2841b87c60d4f07d29bc8f914a5bd596
elescuchante10-code <elescuchante10@gmail.com>
2026-02-08 09:06:21 -0500

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,4 +1,22 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
+# Base declarativa (ya validada en 4.1)
 Base = declarative_base()
+
+# URL de la base de datos
+DATABASE_URL = "sqlite:///./biblioteca.db"
+
+# Engine
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+# Session local
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
